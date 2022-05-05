bf4126587a6f46a587b60dfc4c3e4dde26122e59
sejal <sejalsingh417@gmail.com>
2022-05-05 23:12:18 +0530

    Chocolate Distribution Problem

---
+++ distribution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+     long long findMinDiff(vector<long long> a, long long n, long long m){
+    
+       long long ans=LONG_MAX;
+       
+       for(long long i=0;i+m-1<n;i++){
+           
+           ans=min(ans, a[i+m-1]-a[i] );
+       }
+       
+       return ans;
+       }
+       };
