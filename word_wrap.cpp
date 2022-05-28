c19c40eadc722bd19dc4eccea3b2090e833d31cb
sejal <sejalsingh417@gmail.com>
2022-05-28 23:18:28 +0530

    Word Wrap

---
+++ word_wrap.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+     int solve(vector<int>& nums, int ind, int l, int rl,vector<vector<int>>& dp)
+    {   
+        if(ind==nums.size()-1)
+        { 
+            if(nums[ind]<rl)
+            {
+                
+                return dp[ind][rl] =  0;
+            }
+            return  dp[ind][rl] = rl * rl;
+        }
+        if(dp[ind][rl]!=-1)
+          return dp[ind][rl];
+       if(nums[ind]<rl)
+       {
+           int take   =solve(nums,ind+1,l,rl==l?rl-nums[ind]:rl-nums[ind]-1,dp);
+           int notTake=rl*rl+solve(nums,ind+1,l,l-nums[ind],dp);
+           return dp[ind][rl] = min(take,notTake);
+       }
+       else
+       {
+           return dp[ind][rl] = rl*rl+solve(nums,ind+1,l,l-nums[ind],dp);
+       }
+    }
+    int solveWordWrap(vector<int>nums, int k) 
+    {   vector<vector<int>> dp(nums.size(),vector<int>(k+1,-1));
+        return solve(nums,0,k,k,dp);
+    } 
+}
+};
